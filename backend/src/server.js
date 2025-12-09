a4ba5b7bc35fbad30980950e4b70a24e5c04c9df
qxh1145 <knightdragon184@gmail.com>
2025-12-09 17:53:04 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ app.use("/tasks", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:5001" : "/"
 
 const api = axios.create({
-    BASE_URL
+    baseURL: BASE_URL,
 })
 
 export default api
